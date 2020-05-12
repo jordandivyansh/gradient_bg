7304b5b1e80537c7e50671c0a71b3ee659ab5dd3
jordandivyansh <divyanshkrit@gmail.com>
2020-05-13 02:19:45 +0530

    working js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ var c1 = document.querySelector(".c1");
 var c2 = document.querySelector(".c2");
 var css= document.querySelector("h3");
 var body= document.getElementById("gradient");
+function bgchange(){
+	body.style.background= "radial-gradient( " + c1.value +","+c2.value+")";
+	css.textContent = body.style.background + ";";
+}
 
-function gradientfunction(){
-	body.style.background = "liner-gradient(to right, " + c1.value + ", " + c2.value + ")";
-	css.text
-};
+c1.addEventListener("input", bgchange);
 
-color1.addEventListener("input", gradientfunction);
-color2.addEventListener("input", gradientfunction);
+c2.addEventListener("input", bgchange);
 
+ 
