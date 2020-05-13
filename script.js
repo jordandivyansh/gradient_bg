65e2bc3a00cf2ccbc853e9367f52b66d29767e08
jordandivyansh <44031579+jordandivyansh@users.noreply.github.com>
2020-05-13 21:30:51 +0530

    Add files via upload
    Adding option to switch radial-linear background

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ var c1 = document.querySelector(".c1");
 var c2 = document.querySelector(".c2");
 var css= document.querySelector("h3");
 var body= document.getElementById("gradient");
+var buttonl = document.getElementById("buttonl");
+var buttonr = document.getElementById("buttonr");
 function bgchange(){
 	body.style.background= "radial-gradient( " + c1.value +","+c2.value+")";
 	css.textContent = body.style.background + ";";
 }
+function linear() {
+	body.style.background= "linear-gradient(to right, " + c1.value +","+c2.value+")";
+	css.textContent = body.style.background + ";";
+}
+
 
 c1.addEventListener("input", bgchange);
 
 c2.addEventListener("input", bgchange);
 
- 
+buttonl.addEventListener("click", linear);
+buttonr.addEventListener("click", bgchange);
+
